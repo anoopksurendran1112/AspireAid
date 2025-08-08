6abf98b3e87195963ce6091195264c4b97ecf3c6
RangithBabu <rangithbabu22@gmail.com>
2025-08-08 10:36:57 +0530

    created admin bank details

--- adminModule/views.py
+++ adminModule/views.py
@@ -8,3 +8,11 @@ def adminDashboard(request):
 
 def adminAddProject(request):
     return render(request, "admin-add-project.html")
+
+
+def adminAllBankDetails(request):
+    return render(request, "admin-all-bank-details.html")
+
+
+def adminEditDefaultBankDetails(request):
+    return render(request,"admin-edit-default-bank-details.html")
