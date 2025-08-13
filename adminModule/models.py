2ed0ebb01edeb924a65d1fe15d6c47ce561fa930
Anoop <anoopksurendran2002@gmail.com>
2025-08-13 14:50:32 +0530

    Created tile system layout

--- adminModule/models.py
+++ adminModule/models.py
@@ -24,9 +24,7 @@ class BankDetails(models.Model):
     account_holder_last_name = models.CharField(max_length=100,null=True)
     account_holder_address = models.CharField(max_length=255)
     account_holder_phn_no = models.CharField(max_length=15, blank=True, null=True)
-    account_type = models.CharField(
-        max_length=10, choices=ACCOUNT_TYPES, default='custom', null=True
-    )
+    account_type = models.CharField(max_length=10, choices=ACCOUNT_TYPES, default='custom', null=True)
     bank_name = models.CharField(max_length=100)
     branch_name = models.CharField(max_length=100, blank=True, null=True)
     ifsc_code = models.CharField(max_length=20, blank=True, null=True)
@@ -44,8 +42,7 @@ class Project(models.Model):
     beneficiary = models.ForeignKey(Beneficial, on_delete=models.CASCADE, related_name='projects')
     funding_goal = models.DecimalField(max_digits=10, decimal_places=2)
     tile_value = models.DecimalField(max_digits=10, decimal_places=2)
-    bank_details = models.ForeignKey(BankDetails, on_delete=models.SET_NULL, null=True, blank=True,
-                                     related_name='projects')
+    bank_details = models.ForeignKey(BankDetails, on_delete=models.SET_NULL, null=True, blank=True,related_name='projects')
     created_at = models.DateTimeField(auto_now_add=True)
     closing_date = models.DateTimeField()
     table_status = models.BooleanField(default=True)
--- userModule/models.py
+++ userModule/models.py
@@ -1,25 +1,27 @@
+from django.conf import settings
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from adminModule.models import Project
+
+
 # Create your models here.
 class CustomUser(AbstractUser):
-    email = models.EmailField(
-        null=False,
-        blank=False,
-        help_text="Required. Must be an email address."
-    )
-    phn_no = models.CharField(
-        max_length=15,
-        blank=True,
-        null=True,
-        help_text="Optional. Phone number for contact.")
-    profile_pic = models.ImageField(
-        upload_to='user_profile_pics/',
-        blank=True,
-        null=True,
-        help_text="Optional. User's profile picture."
-    )
+    email = models.EmailField(null=False, blank=False)
+    phn_no = models.CharField(max_length=15, blank=True, null=True)
+    profile_pic = models.ImageField(upload_to='user_profile_pics/', blank=True, null=True)
     table_status = models.BooleanField(default=True)
 
     def __str__(self):
         return f"User: {self.email}"
+
+
+class SelectedTile(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='projects')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='selected_tiles')
+    tiles = models.CharField(max_length=255)
+    funded_at = models.DateTimeField(auto_now_add=True)
+    table_status = models.BooleanField(default=True)
+
+    def __str__(self):
+        return f"Tiles for {self.user.username} on {self.project.title}"
