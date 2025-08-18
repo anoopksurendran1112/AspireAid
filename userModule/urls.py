7a83d853f148892cf4f3e66740e4d82ec654d29c
Anoop <anoopksurendran2002@gmail.com>
2025-08-18 15:27:40 +0530

    Updated Project flow

--- userModule/urls.py
+++ userModule/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('user-dash/',views.userDashboard),
-    path('user-avail-project/',views.userAvailbleProjects),
     path('user-single-project/<int:prj_id>/',views.userSingleProject),
+    path('checkout/', views.userCheckoutView, name='user_checkout'),
 ]
--- userModule/views.py
+++ userModule/views.py
@@ -1,32 +1,38 @@
-from datetime import date
-from django.contrib.auth.decorators import login_required
+import urllib.parse
 from django.shortcuts import render, redirect
+from django.urls import reverse
 from adminModule.models import Project
-from userModule.models import CustomUser
 
 
 # Create your views here.
-@login_required(login_url='/sign-in/')
-def userDashboard(request):
-    if request.user and (request.user.is_superuser == False and request.user.is_staff == False):
-        return render(request, "user-dashboard.html", {'user':request.user})
-    else:
-        return redirect('/sign-in/')
-
-
-def userAvailbleProjects(request):
-    if request.user and (request.user.is_superuser == False and request.user.is_staff == False):
-        admin = CustomUser.objects.filter(is_staff = True, institution=request.user.institution)
-        avail_prj = Project.objects.filter(closing_date__gte=date.today(), created_by__in=admin)
-        return render(request,'user-available-project.html', {'user':request.user, 'projects':avail_prj})
-    else:
-        return redirect('/sign-in/')
-
-
 def userSingleProject(request,prj_id):
     if  request.user and (request.user.is_superuser == False and request.user.is_staff == False):
         prj = Project.objects.get(id=prj_id)
         tile_range = range(1, int(prj.funding_goal // prj.tile_value) + 1)
+
+        if request.method == "POST":
+            selected_tiles = request.POST.get("selected_tiles_input")
+            query_string = urllib.parse.urlencode({
+                'project_id': prj.id,
+                'selected_tiles': selected_tiles
+            })
+            return redirect(reverse('user_checkout') + '?' + query_string)
         return render(request, 'user-single-project.html', {'user': request.user, 'project': prj,'t_range': tile_range})
+    else:
+        return redirect('/sign-in/')
+
+
+def userCheckoutView(request,):
+    if request.user and (request.user.is_superuser == False and request.user.is_staff == False):
+        if request.method == "GET":
+            project_id = request.GET.get("project_id")
+            selected_tiles = request.GET.get("selected_tiles")
+
+            project = Project.objects.get(id=project_id)
+            selected_tile_count = len(selected_tiles.split('-'))
+
+        elif  request.method == "POST":
+            return redirect('/user/user-dash/')
+        return render(request, 'user-checkout.html', {'user': request.user, 'project': project, 'selected_tiles': selected_tiles, 'count':selected_tile_count})
     else:
         return redirect('/sign-in/')
