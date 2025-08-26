717fbc2bc04ba189054990bb9df589f8e1c625b1
Anoop <anoopksurendran2002@gmail.com>
2025-08-26 18:21:00 +0530

    Updated Ui for Userside Projects

--- userModule/urls.py
+++ userModule/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path('index/<int:ins_id>/',views.userIndex),
-    path('about/', views.about),
-    path('contact-us/', views.contact_us),
-    path('all-project/',views.userAllProject),
-    path('single-project/<int:prj_id>/',views.userSingleProject),
-    path('checkout/', views.userCheckoutView, name='user_checkout'),
-    path('track-status/', views.userTrackStatus),
+    path('<int:ins_id>/',views.userIndex),
+    path('<int:ins_id>/about/', views.about),
+    path('<int:ins_id>/contact-us/', views.contact_us),
+    path('<int:ins_id>/all-project/',views.userAllProject),
+    path('<int:ins_id>/single-project/<int:prj_id>/',views.userSingleProject),
+    path('<int:ins_id>/checkout/', views.userCheckoutView, name='user_checkout'),
+    path('<int:ins_id>/track-status/', views.userTrackStatus),
 ]
