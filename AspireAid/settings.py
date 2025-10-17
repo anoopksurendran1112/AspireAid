0319eb6aa81d7d6171fc6d520dee0f5f3f62b578
Anoop <anoopksurendran2002@gmail.com>
2025-10-17 10:25:49 +0530

    Connected mariadb

--- AspireAid/settings.py
+++ AspireAid/settings.py
@@ -76,13 +76,27 @@ WSGI_APPLICATION = 'AspireAid.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'aid',
+#         'USER': 'lion',
+#         'PASSWORD': 'db@7300gt',
+#         'HOST': 'db.kochi.digital',
+#         'PORT': '3306',
+#         'OPTIONS': {
+#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+#             'charset': 'utf8mb4',
+#         }
+#     }
+# }
 
 
 AUTH_USER_MODEL = 'adminModule.CustomUser'
